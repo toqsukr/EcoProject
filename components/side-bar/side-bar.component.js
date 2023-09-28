bbcc5e70c573a89d6a867223840ef1171698dd12
Wastard <Ilya.pakylov@gmail.com>
2023-09-28 15:04:28 +0300

    added sidebar; updated styles

--- components/side-bar/side-bar.component.js
+++ components/side-bar/side-bar.component.js
@@ -3,5 +3,5 @@ import './side-bar.style.css';
 export const sideBar = async () => {
   const moduleUrl = new URL('./side-bar.template.html', import.meta.url);
   const data = await (await fetch(moduleUrl)).text();
-  return data;
+  document.querySelector('#side-bar-container').innerHTML = data;
 };
--- main.js
+++ main.js
@@ -2,9 +2,5 @@ import { header } from './components/header/header.component';
 import { sideBar } from './components/side-bar/side-bar.component';
 import './style.css';
 
-const sideTemplate = sideBar();
-console.log(sideTemplate);
+sideBar();
 header();
-document.querySelector('#app').innerHTML = `
-<section id='main-section'></section>
-`;
